f18a37fe4e83231817f62551ad2964833fc45110
Maria Troyanovskaya <lamarilu@ya.ru>
2023-06-29 01:59:10 +0300

    Изменила 3001 на 3000

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const { handleErrors } = require('./middlewares/handleErrors');
 const app = express();
 const {
   MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb',
-  PORT = 3001,
+  PORT = 3000,
 } = process.env;
 
 app.use(express.json());
